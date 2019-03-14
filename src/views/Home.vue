c74326894e30afc69d4126eb4b0e88858a93ecc6
elisejakob <hei@elisejakob.no>
2019-03-14 11:55:12 +0100

    add infographics

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,55 @@
 <template>
   <div class="home">
-    <transition name="fadeup" appear>
-      <Video
+    <transition name="fade" appear>
+      <Hero
         video="/images/logo/logo-anim-white.mp4"
-        size="full"
+        heading="Annual Report 2018"
+        subhead="RITMO - Centre for Interdisciplinary Studies in Rhythm, Time and Motion"
       />
     </transition>
     <transition name="fadeup" appear>
       <section id="part1" class="text">
-        <h1>Annual Report 2018</h1>
-        <p class="subhead">RITMO - Centre for Interdisciplinary Studies in Rhythm, Time and Motion</p>
         <p class="lead">2018 was the year that RITMO went from idea to reality. Even though many things have changed since we first started planning RITMO in 2012, the core people, the main ideas, and the excitement of being part of something quite unique, is still there!</p>
       </section>
     </transition>
+    <Figure
+      image="/images/infographics/RITMO-Bobler.png"
+      alt="Bildet viser no greier"
+      size="column"
+    />
     <section id="part1" class="text item">
       <h2>Overskrift H2</h2>
       <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.</p>
     </section>
+    <Figure
+      image="/images/infographics/RITMO-Matrise.svg"
+      alt="Bildet viser no greier"
+      size="column"
+    />
+    <Figure
+      image="/images/infographics/RITMO-Partners-land.svg"
+      alt="Bildet viser no greier"
+      size="column"
+    />
+    <Figure
+      image="/images/infographics/RITMO-People-kjonn.svg"
+      alt="Bildet viser no greier"
+      size="column"
+    />
+    <Figure
+      image="/images/infographics/RITMO-People-land.svg"
+      alt="Bildet viser no greier"
+      size="column"
+    />
+    <Figure
+      image="/images/infographics/RITMO-Results.svg"
+      alt="Bildet viser no greier"
+      size="column"
+    />
+    <Video
+      video="/images/logo/logo-anim-white.mp4"
+      size="column"
+    />
     <div class="download item">
       <div class="inner">
         Last ned hele publikasjonen (PDF)
@@ -47,12 +80,16 @@
 
 <script>
 // @ is an alias to /src
+import Hero from '@/components/Hero.vue'
 import Video from '@/components/Video.vue'
+import Figure from '@/components/Figure.vue'
 
 export default {
   name: 'home',
   components: {
-    Video
+    Hero,
+    Video,
+    Figure
   }
 }
 </script>
