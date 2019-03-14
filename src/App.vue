b7898d5b17e22b65130cc2c7c7944fa9a3c9f341
elisejakob <hei@elisejakob.no>
2019-03-14 13:00:41 +0100

    🍔 add burger animation

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <header id="header" class="site-header">
       <div class="logo">UiO</div>
       <nav class="site-nav">
-        <div class="menu" id="main-menu">
+        <div class="menu" :class="{ visible: showmenu }" id="main-menu">
           <ul>
             <!--<li><router-link to="/">Home</router-link></li>-->
             <h3>Annual Report 2018</h3>
@@ -20,9 +20,10 @@
             <li><a href="#" v-scroll-to="'#end'">Ansatte</a></li>
           </ul>
         </div>
-        <div class="menu-toggle" @click="togglemenu()">
-          <template v-if="showmenu">&times;</template>
-          <template v-else>=</template>
+        <div class="menu-toggle" :class="{ open: showmenu }" @click="togglemenu()">
+          <div class="burger-wrapper">
+            <div class="burger"></div>
+          </div>
         </div>
       </nav>
     </header>
@@ -49,7 +50,6 @@ export default {
   },
   methods: {
     togglemenu: function() {
-      document.getElementById('main-menu').classList.toggle('visible');
       this.showmenu = !this.showmenu;
     },
     handleScroll: function() {
@@ -118,8 +118,75 @@ export default {
 
     cursor: pointer;
     position: absolute;
-    top: 1rem;
+    top: 1.5rem;
     right: 1rem;
+
+    transition-timing-function: linear;
+    transition-duration: .15s;
+    transition-property: opacity,filter;
+
+    .burger-wrapper {
+      position: relative;
+      width: 1.6rem;
+      height: 1.6rem;
+      display: block;
+
+      .burger {
+        transition-timing-function: cubic-bezier(.55,.055,.675,.19);
+        transition-duration: 75ms;
+        position: absolute;
+        width: 1.8rem;
+        height: 4px;
+        transition-timing-function: ease;
+        transition-duration: 0.2s;
+        transition-property: transform;
+        border-radius: 4px;
+        background-color: #fff;
+
+        &:before, &:after {
+          display: block;
+          content: "";
+          position: absolute;
+          width: 1.8rem;
+          height: 4px;
+          transition-timing-function: ease;
+          transition-duration: 0.2s;
+          transition-property: transform;
+          border-radius: 4px;
+          background-color: #fff;
+        }
+        &:before {
+          transition: top 75ms ease .12s,opacity 75ms ease;
+          top: -10px;
+        }
+        &:after {
+          transition: bottom 75ms ease .12s,transform 75ms cubic-bezier(.55,.055,.675,.19);
+          bottom: -10px;
+        }
+      }
+    }
+
+    &.open {
+      .burger-wrapper {
+        .burger {
+          transition-delay: 0.16s;
+          transition-timing-function: cubic-bezier(.215,.61,.355,1);
+          transform: rotate(45deg);
+          background-color: #fff;
+
+          &:before {
+            top: 0;
+            transition: top 75ms ease,opacity 75ms ease 0.16s;
+            opacity: 0;
+          }
+          &:after {
+            bottom: 0;
+            transition: bottom 75ms ease,transform 75ms cubic-bezier(.215,.61,.355,1) .12s;
+            transform: rotate(-90deg);
+          }
+        }
+      }
+    }
   }
 
   .menu {
@@ -133,7 +200,7 @@ export default {
     color: $color-white;
     background: $color-theme-dark;
 
-    transform: translateX($width-s);
+    transform: translateX(100vw);
     transition: transform .3s ease-in-out;
 
     &.visible {
